3cada52bff4ae2b29f00ad383c0e495fe95db32d
exact-coder <hasanakash799@gmail.com>
2024-08-04 17:15:46 +0600

    LeeCode 322 Coin Change problem solved

--- LeetCode/322_Coin_Change.py
+++ LeetCode/322_Coin_Change.py
@@ -1,12 +1,20 @@
 
 
-def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
+def coinChange(coins, amount):
+        amt = [amount + 1] * (amount + 1)
+        amt[0] = 0
+        print(amt)
+
+        for i in range(1, amount + 1):
+                for j in range(len(coins)):
+                        if i >= coins[j]:
+                                amt[i] = min(amt[i], 1 + amt[i - coins[j]])
+
+        if amt[amount] < amount + 1:
+                return amt[amount]
+        return -1
 
 print(coinChange([1,2,5],11))
 print(coinChange([2],3))
 print(coinChange([1],0))
+
