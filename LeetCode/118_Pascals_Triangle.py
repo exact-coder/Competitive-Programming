a7080df05dd479b0fb5d7f0a74ab3e9a1398e2e7
exact-coder <hasanakash799@gmail.com>
2024-08-05 21:48:19 +0600

    Codeforces 1913B. Swap and Delete problem solved

---
+++ LeetCode/118_Pascals_Triangle.py
@@ -0,0 +1,12 @@
+
+def generate(self, numRows):
+    """
+    :type numRows: int
+    :rtype: List[List[int]]
+    """
+
+print(generate(5))
+# [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+print(generate(1))
+# [[1]]
+
