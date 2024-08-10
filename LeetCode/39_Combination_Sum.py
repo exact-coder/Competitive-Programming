209f8a7494f6d884e3de892fb6129e1dcca13448
exact-coder <hasanakash799@gmail.com>
2024-08-11 02:06:20 +0600

    LeeCode 39 Combination Sum problem solved

--- LeetCode/39_Combination_Sum.py
+++ LeetCode/39_Combination_Sum.py
@@ -1,11 +1,23 @@
 
 
 def combinationSum(candidates, target):
-    """
-    :type candidates: List[int]
-    :type target: int
-    :rtype: List[List[int]]
-    """
+    res = []
+    combination =[]
+    backTrack(target,res,combination,0,candidates)
+    return res
+
+def backTrack(target,res,combination,start,candidates):
+
+    if target == 0:
+        res.append(list(combination))
+
+    elif target <0:
+        return
+    for i in range(start, len(candidates)):
+        combination.append(candidates[i])
+        backTrack(target - candidates[i], res, combination, i, candidates)
+        combination.pop()
+
 
 print(combinationSum([2,3,6,7],7))
 # [[2,2,3],[7]]
