6505e4e6f7f8fa580710ba24ae5208ec2d951959
exact-coder <hasanakash799@gmail.com>
2024-08-13 16:33:18 +0600

    LeetCode 213 House Robber ii problem solved

--- LeetCode/213_House_Robber_II.py
+++ LeetCode/213_House_Robber_II.py
@@ -1,9 +1,25 @@
 
 def rob(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
+    rob1 =0
+    rob2=0
+    max1=nums[0]
+    max2=0
+    length = len(nums)
+
+    for i in range(length-1):
+        max1 = max(rob1+nums[i],rob2)
+        rob1 = rob2
+        rob2 = max1
+    
+    rob1 =0
+    rob2 =0
+
+    for i in range(1,length):
+        max2 = max(rob1+nums[i],rob2)
+        rob1 = rob2
+        rob2 = max2
+    
+    return max(max1,max2)
 
 print(rob([2,3,2]))
 # 3
