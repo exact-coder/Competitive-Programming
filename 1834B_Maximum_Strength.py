d32913d877ed68ebf455d60268c9f6a589c3cf04
exact-coder <hasanakash799@gmail.com>
2024-09-26 01:42:21 +0600

    codeforces 1834B. Maximum Strength problem trying to solve

---
+++ 1834B_Maximum_Strength.py
@@ -0,0 +1,17 @@
+""" ******************************
+Username: JahidHasan90
+Problem: https://codeforces.com/problemset/problem/1834/B
+Give Time to Time , It can hill everything.
+*******************************"""
+
+
+test_case = int(input())
+
+
+while test_case:
+    
+    L,R = map(int,input().split())
+    
+    
+    test_case-=1
+
