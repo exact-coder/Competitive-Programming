936db86e71989347ee392c32d8e238f6c20c9844
exact-coder <hasanakash799@gmail.com>
2024-10-09 15:33:17 +0600

    codeforces 1992B problem trying to solve

--- Sample.py
+++ Sample.py
@@ -11,23 +11,15 @@ tc = int(input())
 while tc:
     p1,p2,p3 = map(int,input().split())
 
-    maxPQ = []
-    if p1>0:
-        maxPQ.append(p1)
-    if p2>0:
-        maxPQ.append(p2)
-    if p3>0:
-        maxPQ.append(p3)
 
-    while(len(maxPQ)>1):
-        first = maxPQ
-        
+
+
+
     tc-=1
 
 
 
 
-# https://codeforces.com/problemset/problem/1994/B
 # https://codeforces.com/problemset/problem/1993/B
 # https://codeforces.com/problemset/problem/1986/C
 # https://codeforces.com/problemset/problem/1984/B
