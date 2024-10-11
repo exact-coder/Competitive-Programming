7110d99aad5e495497fc7f645f2768fe73948598
exact-coder <hasanakash799@gmail.com>
2024-10-11 18:49:28 +0600

    Codeforces 1996C. Update Queries problem solved

--- CodeForces/1996C_Update_Queries.py
+++ CodeForces/1996C_Update_Queries.py
@@ -10,8 +10,16 @@ tc = int(input())
 
 while tc:
     n,m = map(int,input().split())
+    s_str = list(input())
+    m_li = sorted(set(list(map(int,input().split()))))
+    c_str = sorted(input())
 
+    inx =0
+    for i in m_li:
+        s_str[i-1] = c_str[inx]
+        inx+=1
 
+    print("".join(map(str,s_str)))
 
     tc-=1
 
