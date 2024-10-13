742acfe09c63f543c9ff6f76ccbc33ac85eb84b2
exact-coder <hasanakash799@gmail.com>
2024-10-13 21:59:01 +0600

    codeforces 1982C problem trying to solve

---
+++ 1982C_Boring_Day.py
@@ -0,0 +1,24 @@
+""" ******************************
+Username: JahidHasan90
+Problem: https://codeforces.com/problemset/problem/1982/C
+Give Time to Time , It can hill everything.
+*******************************"""
+
+
+tc = int(input())
+
+
+while tc:
+    n,l,r = map(int,input().split())
+    a_li = list(map(int,input().split()))
+
+
+
+
+
+    tc-=1
+
+
+
+
+
