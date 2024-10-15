64f2a72474c9e2c64b3d3ea7b82a60eb500d4053
exact-coder <hasanakash799@gmail.com>
2024-10-15 23:53:17 +0600

    codeforces 1982C problem trying to solve

--- 1982C_Boring_Day.py
+++ 1982C_Boring_Day.py
@@ -5,20 +5,36 @@ Give Time to Time , It can hill everything.
 *******************************"""
 
 
+
+
 tc = int(input())
 
 
 while tc:
     n,l,r = map(int,input().split())
-    a_li = list(map(int,input().split()))
-
-
-
-
+    a = list(map(int,input().split()))
+    
+    sum_ = 0
+    ans = 0
+    start = 0
+    
+    for i in range(n):
+        if a[i] > r:
+            sum_ = 0
+            start = i + 1
+            continue
+        sum_ += a[i]
+        while sum_ > r:
+            sum_ -= a[start]
+            start += 1
+        if 1 <= sum_ <= r:
+            ans += 1
+            sum_ = 0
+            start = i + 1
+    
+    print(ans)
 
     tc-=1
-
-
 
 
 
