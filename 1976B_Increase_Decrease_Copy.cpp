73334cb66370d9e0a1250f9b81c52eff02dd6856
exact-coder <hasanakash799@gmail.com>
2024-11-05 00:33:27 +0600

    chore: 1976B trying to solve

--- 1976B_Increase_Decrease_Copy.cpp
+++ 1976B_Increase_Decrease_Copy.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 
+
 /* ******************************
 Username: JahidHasan90
 Problem: https://codeforces.com/problemset/problem/1976/B
@@ -9,14 +10,43 @@ Give Time to Time , It can hill everything.
 ********************************/
 
 
+
 int main(){
     int tc; cin>>tc;
     while (tc--)
     {
-        
+        int n; cin>>n;
+        vector<int>a(n),b(n+1);
+
+        for (int i=0; i<a.size(); i++) {
+            cin >> a[i];
+        }
+
+        for (int i=0; i<b.size(); i++) {
+            cin >> b[i];
+        }
+
+        int tot=1;
+        for (int i=0;i<n;i++){
+            tot+=abs(a[i]-b[i]);
+        }
+        int mn = INT_MAX;
+        for(int i=0;i<n;i++){
+            if(a[i]<=b[n] && b[n]<=b[i]) mn=0;
+            if(a[i]>=b[n] && b[n]>=b[i]) mn=0;
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]<=b[n] && a[i]<=b[i]) mn=min(mn,abs(b[n]-b[i]));
+            if(a[i]>=b[n] && a[i]>=b[i]) mn=min(mn,abs(b[n]-b[i]));
+        }
+        for(int i=0;i<n;i++){
+            mn=min(mn,abs(b[n]-a[i]));
+        }
+        cout << tot+mn <<"\n";
     }
     return 0;
 }
+
 
 
 // g++ learn.cpp && ./a.exe
--- 1976B_Increase_Decrease_Copy.py
+++ 1976B_Increase_Decrease_Copy.py
@@ -10,9 +10,35 @@ tc = int(input())
 
 while tc:
     n = int(input())
-    a_l = map(int,input().split())
-    b_l = map(int,input().split())
+    a = map(int,input().split())
+    b = map(int,input().split())
+
+    tot = 1
+    for i in range(n):
+        tot += abs(a[i] - b[i])
+
+    mn = float('inf')
+    for i in range(n):
+        if a[i] <= b[n] and b[n] <= b[i]:
+            mn = 0
+        if a[i] >= b[n] and b[n] >= b[i]:
+            mn = 0
+
+    for i in range(n):
+        if a[i] <= b[n] and a[i] <= b[i]:
+            mn = min(mn, abs(b[n] - b[i]))
+        if a[i] >= b[n] and a[i] >= b[i]:
+            mn = min(mn, abs(b[n] - b[i]))
+
+    for i in range(n):
+        mn = min(mn, abs(b[n] - a[i]))
+
+    print(tot + mn)
 
 
     tc-=1
+
+
+
+
 
