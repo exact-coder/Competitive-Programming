27538e1435c324f949811ee6b18243d634ed5735
Jahid Hasan <jahidhasan@aihcon.com>
2024-12-12 14:46:15 +0600

    added some problemset for letter solved

--- Sample.py
+++ Sample.py
@@ -19,7 +19,19 @@ while tc:
 
 
 
-
-
 # https://codeforces.com/problemset/problem/1951/B
+# https://codeforces.com/problemset/problem/2046/A
+# https://codeforces.com/problemset/problem/2041/E
+# https://codeforces.com/problemset/problem/2041/B
+# https://codeforces.com/problemset/problem/2039/C1
+# https://codeforces.com/problemset/problem/2036/C
+# https://codeforces.com/problemset/problem/2032/B
+# https://codeforces.com/problemset/problem/2030/C
+# https://codeforces.com/problemset/problem/2029/B
+# https://codeforces.com/problemset/problem/2027/B
+# https://codeforces.com/problemset/problem/2025/B
+# https://codeforces.com/problemset/problem/2024/B
+# https://codeforces.com/problemset/problem/2021/B
+# https://codeforces.com/problemset/problem/2020/B
+# https://codeforces.com/problemset/problem/2019/B
 
