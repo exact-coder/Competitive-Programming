ffac5b681130d9f528f7eb0f8950013ca73778b0
exact-coder <hasanakash799@gmail.com>
2025-07-26 18:33:51 +0600

    learning

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const nums = [ 3,4,5,6,7,8,56]
 let sum =nums.reduce((a,b)=>{
-    return a + b
+    return a * b
 },11)
-console.log(sum)
+// console.log(sum)
+
+console.log(nums.map((num,i)=> num * nums[i+1]));
