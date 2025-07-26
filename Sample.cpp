a1e3e74fec139444db0188d7408eef5be78de96e
exact-coder <hasanakash799@gmail.com>
2025-07-26 18:10:31 +0600

    learning

--- Sample.cpp
+++ Sample.cpp
@@ -13,10 +13,14 @@ int main(){
     int tc; cin>>tc;
     while (tc--)
     {
-        
+        int n; cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        sort(a.begin(), a.end());
+        cout << a[0] << " " << a[n - 1] << endl;
     }
     return 0;
 }
 
 
-// g++ learn.cpp && ./a.exe
+// g++ Sample.cpp && ./a.exe
---
+++ test.js
@@ -0,0 +1,5 @@
+const nums = [ 3,4,5,6,7,8,56]
+let sum =nums.reduce((a,b)=>{
+    return a + b
+},11)
+console.log(sum)
